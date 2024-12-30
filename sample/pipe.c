92d2b456506c25a3c77d3881403c6e95c9f469ca
jojotown-eng <jojo5nami8@gmail.com>
2024-12-30 20:16:04 +0900

    パイプとリダイレクトの実装できた.waitの数だけ意味わからない

--- sample/pipe.c
+++ sample/pipe.c
@@ -9,35 +9,82 @@
 #include <sys/stat.h>
 
 int main() {
-    int pipefd[2];
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-
-    if(fork()==0){
-			close(pipefd[0]);
-			dup2(pipefd[1],STDOUT_FILENO);
-			close(pipefd[1]);
-			execvp("ls",(char *[]){"ls",NULL});
-			perror("execlp ls");
-      exit(1);
+	
+	char *cmd[]={"ls",">","output.txt",NULL};
+	int is_redirect=0;
+	int *pipe_locate=NULL;
+	pipe_locate=(int*)malloc(sizeof(int));
+	pipe_locate[0]=-1;
+	int pipe_count=0;
+	
+	for(int i=0;cmd[i]!=NULL;i++){
+		if(strcmp(cmd[i],">")==0){
+			is_redirect=i;
+			cmd[i]=NULL;
+		}else if(strcmp(cmd[i],"|")==0){
+			pipe_count++;
+			pipe_locate=realloc(pipe_locate,(pipe_count+1) * sizeof(int));
+			pipe_locate[pipe_count]=i;
+			cmd[i]=NULL;
 		}
+	}
+
+	int pipefd[pipe_count+1][2];
+
+	if(pipe_count==0 && is_redirect==0){
 		if(fork()==0){
-			close(pipefd[1]);
-			dup2(pipefd[0],STDIN_FILENO);
-			close(pipefd[0]);
-			int fd=open("output.txt", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+			if(execvp(cmd[0],cmd)){
+				perror("execvp");
+				exit(EXIT_FAILURE);
+			}		
+		}
+	}else if(is_redirect!=0 && pipe_count==0){
+		if(fork()==0){
+			int fd=open(cmd[is_redirect+1], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 			dup2(fd, 1);
 			close(fd);
-			execvp("grep",(char *[]){"grep","output",NULL});
+			if(execvp(cmd[0],cmd)){
+				perror("execvp");
+				exit(EXIT_FAILURE);
+			}
 		}
-
-		close(pipefd[0]);
-		close(pipefd[1]);
-
+	}else{
+		for(int i=0;i<pipe_count+1;i++){
+			if(i!=pipe_count){
+				pipe(pipefd[i]);
+			}
+			if(fork()==0){
+				if(i==0){
+					close(pipefd[i][0]);
+					dup2(pipefd[i][1],STDOUT_FILENO);
+					close(pipefd[i][1]);
+				}else if(i==pipe_count){
+					dup2(pipefd[i-1][0],STDIN_FILENO);
+					close(pipefd[i-1][0]);
+					if(is_redirect){
+						int fd=open(cmd[is_redirect+1], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+						dup2(fd, 1);
+						close(fd);
+					}
+					close(pipefd[i-1][1]);
+				}else{
+					close(pipefd[i][0]);
+					close(pipefd[i-1][1]);
+					dup2(pipefd[i-1][0], 0);
+					dup2(pipefd[i][1], 1);
+					close(pipefd[i][1]);
+					close(pipefd[i-1][0]);
+				}
+				if(execvp(cmd[pipe_locate[i] + 1], cmd + pipe_locate[i] + 1) < 0){
+					perror("execvp");
+					exit(EXIT_FAILURE);
+				}
+			}
+		}
+	}
+	for(int i=0;i<pipe_count;i++){
 		wait(NULL);
-		wait(NULL);  
+	}
 
-    return 0;
+	return 0;
 }
