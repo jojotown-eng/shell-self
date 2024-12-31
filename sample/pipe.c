6b6904840c05808064769edfc121a19c2a552dab
jojotown-eng <jojo5nami8@gmail.com>
2025-01-01 01:09:04 +0900

    パイプとリダイレクトの実装、仮

--- sample/pipe.c
+++ sample/pipe.c
@@ -10,22 +10,22 @@
 
 int main() {
 	
-	char *cmd[]={"ls",">","output.txt",NULL};
+	char *argv[]={"ls","|","grep","output",">","output.txt",NULL};
 	int is_redirect=0;
 	int *pipe_locate=NULL;
 	pipe_locate=(int*)malloc(sizeof(int));
 	pipe_locate[0]=-1;
 	int pipe_count=0;
 	
-	for(int i=0;cmd[i]!=NULL;i++){
-		if(strcmp(cmd[i],">")==0){
+	for(int i=0;argv[i]!=NULL;i++){
+		if(strcmp(argv[i],">")==0){
 			is_redirect=i;
-			cmd[i]=NULL;
-		}else if(strcmp(cmd[i],"|")==0){
+			argv[i]=NULL;
+		}else if(strcmp(argv[i],"|")==0){
 			pipe_count++;
 			pipe_locate=realloc(pipe_locate,(pipe_count+1) * sizeof(int));
 			pipe_locate[pipe_count]=i;
-			cmd[i]=NULL;
+			argv[i]=NULL;
 		}
 	}
 
@@ -33,17 +33,17 @@ int main() {
 
 	if(pipe_count==0 && is_redirect==0){
 		if(fork()==0){
-			if(execvp(cmd[0],cmd)){
+			if(execvp(argv[0],argv)){
 				perror("execvp");
 				exit(EXIT_FAILURE);
 			}		
 		}
 	}else if(is_redirect!=0 && pipe_count==0){
 		if(fork()==0){
-			int fd=open(cmd[is_redirect+1], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+			int fd=open(argv[is_redirect+1], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 			dup2(fd, 1);
 			close(fd);
-			if(execvp(cmd[0],cmd)){
+			if(execvp(argv[0],argv)){
 				perror("execvp");
 				exit(EXIT_FAILURE);
 			}
@@ -62,7 +62,7 @@ int main() {
 					dup2(pipefd[i-1][0],STDIN_FILENO);
 					close(pipefd[i-1][0]);
 					if(is_redirect){
-						int fd=open(cmd[is_redirect+1], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+						int fd=open(argv[is_redirect+1], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 						dup2(fd, 1);
 						close(fd);
 					}
@@ -75,7 +75,7 @@ int main() {
 					close(pipefd[i][1]);
 					close(pipefd[i-1][0]);
 				}
-				if(execvp(cmd[pipe_locate[i] + 1], cmd + pipe_locate[i] + 1) < 0){
+				if(execvp(argv[pipe_locate[i] + 1], argv + pipe_locate[i] + 1) < 0){
 					perror("execvp");
 					exit(EXIT_FAILURE);
 				}
